3e01bc19e697e86ce4d298ebfe52041cdcda0e5b
Alexandru Loghin <alexandru.loghin@endava.com>
2019-05-24 14:16:08 +0300

    first changes

--- app/src/main/java/com/pluralsight/candycoded/Candy.java
+++ app/src/main/java/com/pluralsight/candycoded/Candy.java
@@ -3,7 +3,7 @@ package com.pluralsight.candycoded;
 public class Candy {
     public int id;
     public String name;
-    public String image;
-    public String price;
-    public String description;
+    String image;
+    String price;
+    String description;
 }
